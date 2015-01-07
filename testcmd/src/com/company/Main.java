57a85d130d39693977a66f6a3adce9d56bdc2371
ani <sharma.animesh@gmail.com>
2015-01-07 18:13:28 +0100

    check time test

---
+++ testcmd/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.TimeZone;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        long time1= (System.currentTimeMillis());
+        System.out.println("TimeC"+time1);
+    }
+}
